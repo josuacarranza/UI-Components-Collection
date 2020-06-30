3e3eb4117bfeb63d749e8e19d1d6f278b1c94dbf
Josua96 <joshuaenrique96@gmail.com>
2020-06-29 20:22:25 -0600

    npm package setup

---
+++ src/input/Input.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './input.scss';
+
+const Input: React.FC<{}> =  () => {
+    return(
+        <h1 className='input'> hola </h1>
+    );
+}
+
+export default Input;
