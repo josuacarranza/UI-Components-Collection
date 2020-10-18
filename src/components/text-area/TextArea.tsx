7bcb3dca216482b617ec708583e19ae351395607
Josua96 <joshuaenrique96@gmail.com>
2020-10-17 20:02:01 -0600

    adding text area and updating some components

---
+++ src/components/text-area/TextArea.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import classNames from "classnames";
+import "./text_area.scss";
+
+interface TextProps {
+  className?: string;
+  errorState?: boolean;
+  size?: "sm" | "md" | "lg";
+  value?: any;
+  placeHolder?: string;
+  defaultValue?: string;
+  disabled?: boolean;
+  bordered?: boolean;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+const baseClass = "UI_collection_text_area";
+
+const TextArea: React.FC<TextProps> = ({
+  className,
+  errorState,
+  bordered = true,
+  size,
+  placeHolder,
+  value,
+  defaultValue,
+  onChange,
+  disabled,
+  ...otherProps
+}) => {
+  const sizeClass = `inp__${size}`;
+  const classes = classNames(baseClass, className, sizeClass, {
+    ["inp__bordered"]: bordered,
+    ["inp__unbordered"]: !bordered,
+    ["inp__error"]: errorState,
+  });
+
+  return (
+    <textarea
+      className={classes}
+      value={value || defaultValue}
+      placeholder={placeHolder}
+      onChange={onChange}
+      disabled={disabled}
+      {...otherProps}
+    ></textarea>
+  );
+};
+
+export default TextArea;
--- src/icons/iconsMap.ts
+++ src/icons/iconsMap.ts
@@ -1,33 +1,37 @@
-import userIcon from './user';
-import usersCircle from './userCircle';
-import arrowLeftIcon from './arrowLeft';
-import star from './star';
-import arrowDown from './arrowDown';
-import arrowUp from './arrowUp';
-import trash from './trash';
-import pause from './pause';
-import play from './play';
-import cloudUpload from './cloudUpload';
-import search from './search';
-import closeTimes from './closeTimes';
-import terminal from './terminal';
-import redo from './redo';
-import questionCircle from './questionCircle';
+import userIcon from "./user";
+import usersCircle from "./userCircle";
+import arrowLeftIcon from "./arrowLeft";
+import star from "./star";
+import arrowDown from "./arrowDown";
+import arrowUp from "./arrowUp";
+import trash from "./trash";
+import pause from "./pause";
+import play from "./play";
+import cloudUpload from "./cloudUpload";
+import search from "./search";
+import closeTimes from "./closeTimes";
+import terminal from "./terminal";
+import redo from "./redo";
+import questionCircle from "./questionCircle";
+import copy from "./copy";
+import plus from "./plus";
 
 export default {
-    "arrowLeft": arrowLeftIcon,
-    "user": userIcon,
-    "userCircle": usersCircle,
-    "star": star,
-    "arrownDown": arrowDown,
-    "arrowUp": arrowUp,
-    "trash": trash,
-    "pause": pause,
-    "play": play,
-    "cloudUpload": cloudUpload,
-    "search": search,
-    "closeTimes": closeTimes,
-    "terminal": terminal,
-    "redo": redo,
-    "questionCircle": questionCircle,
-}
+  arrowLeft: arrowLeftIcon,
+  user: userIcon,
+  userCircle: usersCircle,
+  star: star,
+  arrownDown: arrowDown,
+  arrowUp: arrowUp,
+  trash: trash,
+  pause: pause,
+  play: play,
+  cloudUpload: cloudUpload,
+  search: search,
+  closeTimes: closeTimes,
+  terminal: terminal,
+  redo: redo,
+  questionCircle: questionCircle,
+  copy: copy,
+  plus: plus,
+};
