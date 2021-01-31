a91e86d537c688982f9d0fa45f88367f6d171f52
Josua96 <joshuaenrique96@gmail.com>
2021-01-30 22:22:42 -0600

    text component update

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -3,12 +3,12 @@ import classNames from "classnames";
 import "./text.scss";
 
 interface TextProps {
-  className?: string,
-  size?: "sm" | "sm2" | "sm3" | "md" | "md2" | "md3" | "md4" | "lg" | "lg2",
-  color?: "primary" | "success" | "secondary" | "danger" | "warning",
-  bold?: boolean,
-  link?: boolean,
-  value: Number | String,
+  className?: string;
+  size?: "sm" | "sm2" | "sm3" | "md" | "md2" | "md3" | "md4" | "lg" | "lg2";
+  color?: "primary" | "success" | "secondary" | "danger" | "warning";
+  bold?: boolean;
+  link?: boolean;
+  value: Number | String;
 }
 
 const baseClass = "UI_collection_text";
@@ -21,25 +21,25 @@ const Text: React.FC<TextProps> = ({
   bold = false,
   value = "",
   link = false,
+  children,
   ...otherProps
 }) => {
   const colorClass = `font__${color}`;
   const classes = classNames(baseClass, className, colorClass, size, {
-      ['font__bold'] : bold,
-      [linkBaseClass]: link,
+    ["font__bold"]: bold,
+    [linkBaseClass]: link,
   });
-  if (!link) {
+  if (!link && value) {
+    return <label className={classes}> {value} </label>;
+  } else if (value) {
     return (
-        <label className={classes}> {value} </label>
-      );
+      <a className={classes} {...otherProps}>
+        {value}
+      </a>
+    );
+  } else {
+    return <label className={classes}> {children} </label>;
   }
-
-  else {
-      return (<a className={classes} {...otherProps}>
-          {value}
-      </a>);
-  }
-  
 };
 
 export default Text;
